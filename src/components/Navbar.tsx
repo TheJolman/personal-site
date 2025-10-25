6c7bb76b9533fc9fb55b84a1743a1339778357f1
Josh Holman <josh@jolman.me>
2025-10-25 02:21:01 -0700

    home page

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export function Navbar() {
+  return (
+    <div className="navbar bg-primary text-primary-content">
+      <button className="btn btn-ghost text-xl">daisyUI</button>
+    </div>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { LocationProvider, Router, Route, hydrate, prerender as ssr } from 'preact-iso';
 
-import { Header } from './components/Header.jsx';
+import { Navbar } from './components/Navbar.jsx';
 import { Home } from './pages/Home/index.jsx';
 import { NotFound } from './pages/_404.jsx';
 import './style.css';
@@ -8,7 +8,7 @@ import './style.css';
 export function App() {
 	return (
 		<LocationProvider>
-			<Header />
+			<Navbar />
 			<main>
 				<Router>
 					<Route path="/" component={Home} />
