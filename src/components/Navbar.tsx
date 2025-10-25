dd207f7ad744e556769149bbd40547c48fbb9b28
Josh Holman <josh@jolman.me>
2025-10-25 03:38:38 -0700

    navbar done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,8 @@
 export function Navbar() {
   return (
     <div className="navbar bg-primary text-primary-content">
-      <button className="btn btn-ghost text-xl">daisyUI</button>
+      <a href="/" className="btn btn-accent text-xl">Home</a>
+      <span className="font-bold absolute text-xl left-1/2 transform -translate-x-1/2">Josh Holman</span>
     </div>
   )
 }
