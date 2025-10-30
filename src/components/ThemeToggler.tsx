270514c692b8b8748201d6d2bffb38fd93dc99f9
Josh Holman <josh@jolman.me>
2025-10-30 09:24:10 -0700

    update theme toggler styling

--- src/components/ThemeToggler.tsx
+++ src/components/ThemeToggler.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'preact/hooks';
 import { themeChange } from 'theme-change';
+import { Sun, Moon } from 'lucide-preact';
 
 export function ThemeToggler() {
   const [theme, setTheme] = useState('coffee');
@@ -13,13 +14,16 @@ export function ThemeToggler() {
   }
 
   return (
-    <input type="checkbox"
-      onClick={toggleTheme}
-      checked={theme === 'caramellatte'}
-      className="toggle"
-      data-toggle-theme="coffee,caramellatte"
-      data-act-class="ACTIVECLASS"
-    />
+    <label className="flex items-center gap-2 cursor-pointer">
+      <Moon size={18}/>
+      <input type="checkbox"
+        onClick={toggleTheme}
+        checked={theme === 'caramellatte'}
+        className="toggle"
+        data-toggle-theme="coffee,caramellatte"
+        data-act-class="ACTIVECLASS"
+      />
+      <Sun size={18}/>
+    </label>
   )
-
 }
